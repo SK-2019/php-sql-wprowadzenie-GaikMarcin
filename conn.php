987a6ed0f07e46677354b4d0564fc2ef26e62ab4
GaikMarcin <73532416+GaikMarcin@users.noreply.github.com>
2020-10-30 08:48:33 +0100

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "mysql-marcin-gaik.alwaysdata.net";
+$username = "217182";
+$password = "marcin1803";
+$dbname = "marcin-gaik_php";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * from pracownicy";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
