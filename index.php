7195f9fd4eeece5b9cafc4add76f1e9b17be6304
GaikMarcin <73532416+GaikMarcin@users.noreply.github.com>
2020-10-30 12:58:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <h1>Marcin Gaik 2Ti</h1>
 <?php
+   require_once('conn.php');
    $conn = new mysqli($servername, $username, $password, $dbname); 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
