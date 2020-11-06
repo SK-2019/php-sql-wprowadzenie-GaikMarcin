68902b3209d9a61657904cdd0f78305a71ec56ff
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-06 14:42:28 +0100

    update

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -55,121 +55,22 @@
             }
         echo("</table>");
 echo("<hr />");
-
-            function robot_pracownicy($nr_zad, $f_sql){
-                $conn = new mysqli("mysql-marcin-gaik.alwaysdata.net", "217182", "Marcin123", "marcin-gaik_php");
-                $sql=$f_sql;
-                $result=$conn->query($sql);
-                    echo("<table border=1>");
-                    echo("<h3>ZAD $nr_zad</h3>");
-                    echo("<li>SQL: $sql");
-                    echo("<th>id</th>");
-                    echo("<th>imię</th>");
-                    echo("<th>dział</th>");
-                    echo("<th>zarobki</th>");
-                        while($row=$result->fetch_assoc()){
-                            echo("<tr>");
-                                echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
-                            echo("</tr>");
-                        }
-                    echo("</table>");
-                    echo("<hr />");
-                    }
+            
     
-
-            function robot_avg($nr_zad, $f_sql){
-                $conn = new mysqli("mysql-marcin-gaik.alwaysdata.net", "217182", "Marcin123", "marcin-gaik_php");
-                $sql=$f_sql;
-                $result=$conn->query($sql);
-                    echo("<table border=1>");
-                    echo("<h3>ZAD $nr_zad</h3>");
-                    echo("<li>SQL: $sql");
-                    echo("<th>dział</th>");
-                    echo("<th>średnia</th>");
-                        while($row=$result->fetch_assoc()){
-                            echo("<tr>");
-                                echo("<td>".$row["dzial"]."</td><td>".$row["srednia"]."</td>");
-                            echo("</tr>");
-                            }
-                    echo("</table>");
-                    echo("<hr />");
-                }
-
-            function robot_count($nr_zad, $f_sql){
-                $conn = new mysqli("mysql-marcin-gaik.alwaysdata.net", "217182", "Marcin123", "marcin-gaik_php");
-                $sql=$f_sql;
-                $result=$conn->query($sql);
-                    echo("<table border=1>");
-                    echo("<h3>ZAD $nr_zad</h3>");
-                    echo("<li>SQL: $sql");
-                    echo("<th>dział</th>");
-                    echo("<th>ilość</th>");
-                        while($row=$result->fetch_assoc()){
-                            echo("<tr>");
-                                echo("<td>".$row["dzial"]."</td><td>".$row["ilosc"]."</td>");
-                            echo("</tr>");
-                        }
-                    echo("</table>");
-                    echo("<hr />");
-                    }
-    
-
-            function robot_sum($nr_zad, $f_sql){
-                $conn = new mysqli("mysql-marcin-gaik.alwaysdata.net", "217182", "Marcin123", "marcin-gaik_php");
-                $sql=$f_sql;
-                $result=$conn->query($sql);
-                    echo("<table border=1>");
-                    echo("<h3>ZAD $nr_zad</h3>");
-                    echo("<li>SQL: $sql");
-                    echo("<th>dział</th>");
-                    echo("<th>suma</th>");
-                        while($row=$result->fetch_assoc()){
-                            echo("<tr>");
-                                echo("<td>".$row["dzial"]."</td><td>".$row["suma"]."</td>");
-                            echo("</tr>");
-                            }
-                    echo("</table>");
-                    echo("<hr />");
-                    }
-                    
-            function robot_min($nr_zad, $f_sql){
-                    $conn = new mysqli("mysql-marcin-gaik.alwaysdata.net", "217182", "Marcin123", "marcin-gaik_php");
-                    $sql=$f_sql;
-                    $result=$conn->query($sql);
-                        echo("<table border=1>");
-                        echo("<h3>ZAD $nr_zad</h3>");
-                        echo("<li>SQL: $sql");
-                        echo("<th>dział</th>");
-                        echo("<th>minimalne</th>");
-                            while($row=$result->fetch_assoc()){
-                                echo("<tr>");
-                                    echo("<td>".$row["dzial"]."</td><td>".$row["minimalne"]."</td>");
-                                echo("</tr>");
-                                }
-                        echo("</table>");
-                        echo("<hr />");
-                        }
-    
-        function robot_max($nr_zad, $f_sql){
-            $conn = new mysqli("mysql-marcin-gaik.alwaysdata.net", "217182", "Marcin123", "marcin-gaik_php");
-            $sql=$f_sql;
-            $result=$conn->query($sql);
+            $sql=('SELECT nazwa_dzial,dzial,avg(zarobki) as srednia from pracownicy,organizacja where dzial=id_org');
+            $result=$conn->query($sql); //mysql
+                echo("<h3>ZAD 4</h3>");//nazwa nad tabelą
                 echo("<table border=1>");
-                echo("<h3>ZAD $nr_zad</h3>");
                 echo("<li>SQL: $sql");
-                echo("<th>dział</th>");
-                echo("<th>maksymalne</th>");
+                echo("<th>srednia</th>");
+                echo("<th>nazwa działu</th>");
                     while($row=$result->fetch_assoc()){
-                     echo("<tr>");
-                        echo("<td>".$row["dzial"]."</td><td>".$row["maksymalne"]."</td>");
-                     echo("</tr>");
+                        echo("<tr>");
+                            echo("<td>".$row['srednia']."</td><td>".$row['nazwa_dzial']."</td>");
+                        echo("</tr>");
                     }
                 echo("</table>");
-                echo("<hr />");
-            }
-            
-            
-robot_avg(4,'SELECT dzial,avg(zarobki) as srednia from pracownicy');
+        echo("<hr />");
 
 $sql=('SELECT nazwa_dzial,avg(zarobki) as srednia from pracownicy,organizacja where dzial=4 and dzial=id_org group by nazwa_dzial');
         $result=$conn->query($sql); //mysql
