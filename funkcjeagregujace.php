7a61f9eb25b57fcf470c73b620e33603f9d887b8
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-06 14:19:01 +0100

    update

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -198,19 +198,7 @@ $sql=('SELECT nazwa_dzial,avg(zarobki) as srednia from pracownicy,organizacja wh
             }
         echo("</table>");
 echo("<hr />");
-$sql=('SELECT count(imie)as ilosc from pracownicy where imie like "%a" and dzial=1 or dzial=3');
-    $result=$conn->query($sql); //mysql
-        echo("<h3>ZAD 7</h3>");//nazwa nad tabelą
-        echo("<table border=1>");
-        echo("<li>SQL: $sql");
-        echo("<th>ilosc</th>");
-            while($row=$result->fetch_assoc()){
-                echo("<tr>");
-                    echo("<td>".$row['ilosc']."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-echo("<hr />");
+            robot_count(7,'SELECT count(imie) as ilosc from pracownicy');
             robot_sum(8,'SELECT dzial,sum(zarobki)as suma from pracownicy group by dzial');
             robot_count(9,'SELECT dzial,count(imie) as ilosc from pracownicy group by dzial');
             robot_avg(10,'SELECT dzial,avg(zarobki) as srednia from pracownicy group by dzial');
