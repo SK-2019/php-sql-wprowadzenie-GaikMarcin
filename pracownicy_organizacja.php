8132154a2731cd7ee26821cddd9bfa5ed76f0218
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-06 15:50:51 +0100

    update

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -188,4 +188,27 @@
                     }
                 echo("</table>");
                 echo("<hr />");
+
+                echo("<h2>Limit</h2>");
+                echo("<hr />");
+
+                cho("<h3>Dwóch najlepiej zarabiających pracowników z działu 4</h3>");
+        $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org and dzial=4 group by nazwa_dzial order by zarobki asc LIMIT 2 LIMIT 2');
+            $result=$conn->query($sql);//mysql
+                echo("<table border=1>");
+                echo("<li>SQL: $sql");
+                echo("<th>id</th>");
+                echo("<th>imie</th>");
+                echo("<th>dzial</th>");
+                echo("<th>zarobki</th>");
+                echo("<th>nazwa_dzial</th>");
+                echo("<th>data_urodzenia</th>");
+                    while($row=$result->fetch_assoc()){
+                        echo("<tr>");
+                            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                        echo("</tr>");
+                    }
+                echo("</table>");
+                echo("<hr />");
+
 ?>
