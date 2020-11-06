86c13593c65ede50ad2e805da77aaf77cef3fd6f
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-06 16:12:30 +0100

    update

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -212,7 +212,7 @@
                 echo("<hr />");
 
                 cho("<h3>Trzy najlepiej zarabiające kobiety z działu 4 i 2</h3>");
-        $sql=('SELECT * FROM pracownicy,organizacja where imie like "%a" and dzial=id_org and dzial=4 or dzial=2  order by zarobki asc LIMIT 2');
+        $sql=('SELECT * FROM pracownicy where imie like "%a" and dzial=4 or dzial=2  order by zarobki asc LIMIT 2');
             $result=$conn->query($sql);//mysql
                 echo("<table border=1>");
                 echo("<li>SQL: $sql");
@@ -220,11 +220,10 @@
                 echo("<th>imie</th>");
                 echo("<th>dzial</th>");
                 echo("<th>zarobki</th>");
-                echo("<th>nazwa_dzial</th>");
                 echo("<th>data_urodzenia</th>");
                     while($row=$result->fetch_assoc()){
                         echo("<tr>");
-                            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
                         echo("</tr>");
                     }
                 echo("</table>");
