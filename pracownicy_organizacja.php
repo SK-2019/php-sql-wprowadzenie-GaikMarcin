17c0cc62d75855f262f36631921ea376eb9a75c5
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-06 15:26:50 +0100

    update

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -50,4 +50,22 @@
                     }
                 echo("</table>");
                 echo("<hr />");
+                
+                echo("<h3>Lista kobiet z nazwami działów</h3>");
+        $sql=('SELECT * FROM pracownicy,organizacja where imie like "%a" and dzial=id_org group by nazwa_dzial');
+            $result=$conn->query($sql);//mysql
+                echo("<table border=1>");
+                echo("<th>id</th>");
+                echo("<th>imie</th>");
+                echo("<th>dzial</th>");
+                echo("<th>zarobki</th>");
+                echo("<th>nazwa_dzial</th>");
+                echo("<th>data_urodzenia</th>");
+                    while($row=$result->fetch_assoc()){
+                        echo("<tr>");
+                            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                        echo("</tr>");
+                    }
+                echo("</table>");
+                echo("<hr />");
 ?>
