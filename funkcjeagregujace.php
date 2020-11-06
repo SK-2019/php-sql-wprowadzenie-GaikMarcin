c013065402ba4643e7ac4d215c6bccd8900d7a87
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-06 13:56:17 +0100

    update

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -26,7 +26,7 @@
                     }
                 echo("</table>");
         echo("<hr />");
-        $sql=('SELECT nazwa_dzial,sum(zarobki) as suma from pracownicy,organizacja where imie like "%a" and dzial=nazwa_dzial');
+        $sql=('SELECT nazwa_dzial,sum(zarobki) as suma from pracownicy,organizacja where imie like "%a" and dzial=id_org');
         $result=$conn->query($sql); //mysql
             echo("<h3>ZAD 2</h3>");//nazwa nad tabelą
             echo("<table border=1>");
