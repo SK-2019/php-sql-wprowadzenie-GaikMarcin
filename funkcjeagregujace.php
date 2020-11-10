9b48dc7039b29be836fb599056a0e6bc15c965e3
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-10 14:35:03 +0100

    update

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -180,4 +180,18 @@ echo("<hr />");
             echo("<hr />");
 
             
+            $sql=('SELECT sum(zarobki) as suma, nazwa_dzial from pracownicy, organizacja where dzial=id_org group by nazwa_dzial having sum(zarobki)<28');
+    $result=$conn->query($sql); //mysql
+        echo("<h3>Suma zarobków w poszczególnych działach mniejsza od 28</h3>");//nazwa nad tabelą
+        echo("<table border=1>");
+        echo("<li>SQL: $sql");
+        echo("<th>suma</th>");
+        echo("<th>nazwa działu</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>".$row['suma']."</td><td>".$row['nazwa_dzial']."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+echo("<hr />");
 ?>
