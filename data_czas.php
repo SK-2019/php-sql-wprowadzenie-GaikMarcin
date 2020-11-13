ff7af61074e84cdabb110c7a2fabab1c67efa016
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-13 09:07:14 +0100

    update

--- data_czas.php
+++ data_czas.php
@@ -225,20 +225,15 @@
                         echo("<h2>Formatowanie Dat</h2>");
             echo("<hr />");
 
-                        echo("<h3>Wyświetl nazwy dni w dacie urodzenia</h3>");
-            $sql=("SELECT *, DATE_FORMAT(data_urodzenia,'-D') from pracownicy,organizacja where dzial=id_org");
+                        echo("<h3>Obecna, dokładna godzina (z dokładnością do milisekund)</h3>");
+            $sql=('SELECT curtime(4)');
                     $result=$conn->query($sql);//mysql
                         echo("<table border=1>");
                         echo("<li>SQL: $sql");
-                        echo("<th>id</th>");
-                        echo("<th>imie</th>");
-                        echo("<th>dzial</th>");
-                        echo("<th>zarobki</th>");
-                        echo("<th>nazwa_dzial</th>");
-                        echo("<th>data_urodzenia</th>");
+                        echo("<th>data i godzina</th>");
                             while($row=$result->fetch_assoc()){
                                 echo("<tr>");
-                                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                                    echo("<td>".$row['data_urodzenia']."</td>");
                                 echo("</tr>");
                             }
                         echo("</table>");
