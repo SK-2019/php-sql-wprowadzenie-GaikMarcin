56d19155f9b5253e9bf1fcaa982163062edebb00
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-18 14:38:21 +0100

    update

--- strona.php
+++ strona.php
@@ -6,4 +6,14 @@ echo("jesteś na stronie.php");
 echo("<li>imię:".$_POST["imie"]);
 echo("<li>nazwisko:".$_POST["nazwisko"]);
 
+   require_once('conn.php');
+  $sql = "INSERT INTO MyGuests (imie, nazwisko)
+  VALUES ('John', 'Doe')";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
 ?>
