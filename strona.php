19b3bbcccf63111192484044404a8ec1769a83ac
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-18 22:29:12 +0100

    update

--- strona.php
+++ strona.php
@@ -7,7 +7,7 @@ echo("<li>imię:".$_POST["imie"]);
 echo("<li>nazwisko:".$_POST["nazwisko"]);
 
    require_once('conn.php');
-   $sql="INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (null,'John',2,60,"1980-12-12");"  
+   $sql="INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (null,'John',2,60,'1980-12-12');"  
   if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
   } else {
