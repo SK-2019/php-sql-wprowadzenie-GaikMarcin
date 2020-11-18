fcb5ccded44005b387136ed78acbff378731d9af
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-18 14:29:43 +0100

    update

---
+++ formularz.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<form action="strona.php" method="POST">
+     <input type="text" name="imie" placeholder="imie"></br>
+     <input type="text" name="nazwisko" placeholder="nazwisko"></br>
+     <input type="submit" value="wyslij do strona.php">
+  </form>
+  
+  <?php
+  $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+  VALUES ('John', 'Doe', 'john@example.com')";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+  ?>
+
+
