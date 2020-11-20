8b9e93a495ff5ce612533833c543880967dd54ad
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-20 10:30:31 +0100

    update

---
+++ delete.php
@@ -0,0 +1,9 @@
+<?php
+require_once("connect.php");
+$sql =  "DELETE FROM pracownicy where id_pracownicy='".$_POST['id']."'";
+echo($sql);
+mysqli_query($conn,$sql);
+mysqli_close($conn);
+header("location:http://php-marcin-gaik.herokuapp.com/formularz.php");
+
+?>
---
+++ formularz.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<form action="strona.php" method="POST">
+   <input type="text" name="imie" placeholder="Imie">
+   <br><input type="text" name="dzial" placeholder="Dzial">
+   <br><input type="text" name="zarobki" placeholder="Zarobki">
+   <br><input type="date" name="data_">
+   <input type="submit" value="Dodaj">
+   </form>
+
+</html>
+<?php
+require_once("conn.php");
+$result=$conn->query("Select * from pracownicy");
+echo("<table border=1>");
+    echo("<th>Id</th>");
+    echo("<th>Imie</th>");
+    echo("<th>Dzial</th>");
+    echo("<th>Zarobki</th>");
+    echo("<th>Data_urodzenia</th>");
+    echo("<th class=usuń>Usuń</th>");
+        while($row=$result->fetch_assoc()){
+            echo("<tr>");
+            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>");
+            echo("<td><form action=delete.php method=POST>");
+            echo("<input type='hidden' name='id' value=".$row['id_pracownicy'].">");
+            echo("<input type=submit value=X>");
+            echo("</form></td>");
+            echo("</tr>");}
+echo("</table>");
+?>
+
