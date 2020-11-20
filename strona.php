6a29aaed7bcb27aef5e01e0476f91512b5858f62
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-20 09:09:44 +0100

    update

--- strona.php
+++ strona.php
@@ -18,22 +18,23 @@ echo("<li>data urodzenia:".$_POST["data_"]);
   }
  
 
-  $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org');
-    $result=$conn->query($sql);
-        echo("<hr />");
-        echo("<h3>Tabela Pracowników</h3>");
-        echo("<li>SQL: $sql");
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imię</th>");
-        echo("<th>dział</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>nazwa działu</th>");
-        echo("<th>data urodzenia</th>");
-            while($row=$result->fetch_assoc()){
-                echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
-                echo("</tr>");
-            }
+  require_once("connect.php");
+    $result=$conn->query("Select * from pracownicy");
+                echo("<table border=1>");
+                    echo("<th>Id</th>");
+                    echo("<th>Imie</th>");
+                    echo("<th>Dzial</th>");
+                    echo("<th>Zarobki</th>");
+                    echo("<th>Data_urodzenia</th>");
+                    echo("<th class=usuń>Usuń</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>");
+                            echo("<td><form action=delete.php method=POST>");
+                            echo("<input type='hidden' name='id' value=".$row['id_pracownicy'].">");
+                            echo("<input type=submit value=X>");
+                            echo("</form></td>");
+                            echo("</tr>");}
+                echo("</table>");            
  $conn->close();
  ?>
