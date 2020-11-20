cf3bb992460edec7eac5b561d28b0706abbfee8d
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-20 08:28:48 +0100

    update

--- strona.php
+++ strona.php
@@ -4,11 +4,12 @@
 
 echo("jesteś na stronie.php");
 echo("<li>imię:".$_POST["imie"]);
-echo("<li>nazwisko:".$_POST["nazwisko"]);
+echo("<li>dział:".$_POST["dzial"]);
+echo("<li>zarobki:".$_POST["zarobki"]);
+echo("<li>data urodzenia:".$_POST["data_"]);
 
    require_once('conn.php');
-   $sql="INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (null,'John',2,60,'1980-12-12');"  
-  if ($conn->query($sql) === TRUE) {
+    {
     echo "New record created successfully";
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
