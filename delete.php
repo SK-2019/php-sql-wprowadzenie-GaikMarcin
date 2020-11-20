ddbc99f81f3f9ba58cccd6c1f5baa7f621397018
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-20 13:04:42 +0100

    update

--- delete.php
+++ delete.php
@@ -4,4 +4,6 @@ $sql =  "DELETE FROM pracownicy where id_pracownicy='".$_POST['id']."'";
 echo($sql);
 mysqli_query($conn,$sql);
 mysqli_close($conn);
+header("location:http://php-marcin-gaik.herokuapp.com/formularz.php");
+
 ?>
