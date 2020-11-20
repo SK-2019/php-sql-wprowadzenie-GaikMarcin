c9bdbcf891c4ee28ceafdceb11a834715d1c1437
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-20 12:55:05 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,6 +4,4 @@ $sql =  "DELETE FROM pracownicy where id_pracownicy='".$_POST['id']."'";
 echo($sql);
 mysqli_query($conn,$sql);
 mysqli_close($conn);
-header("location:http://php-marcin-gaik.herokuapp.com/formularz.php");
-
 ?>
