700072789f4cd8629422bf842f1d484e23761237
GaikMarcin <marcin.gaik5@gmail.com>
2020-11-24 13:00:04 +0100

    update

--- function.php
+++ function.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="style1.css">
+<a href="https://github.com/SK-2019/php-sql-wprowadzenie-GaikMarcin"> GitHub </a>
+
+   <div class="nav">
+    <a class="nav_link" href="index.php">Strona Główna</a> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
 
 function petla(){
--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <a class="nav_link" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
     <a class="nav_link" href="data_czas.php">Data i Czas</a>
     <a class="nav_link" href="insert.php">Formularz</a>
+    <a class="nav_link" href="function.php">Function</a>
 </div>
 </head>
 <body>
