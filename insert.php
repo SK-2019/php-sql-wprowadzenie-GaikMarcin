84ecd8ba335ea5aab1a70a8e6c5003991e0ec607
GaikMarcin <marcin.gaik5@gmail.com>
2020-12-02 15:51:36 +0100

    update

--- insert.php
+++ insert.php
@@ -7,5 +7,5 @@ $sql_autor="INSERT INTO `BiblAutor`(`id_autor`, `imie`, `nazwisko`) VALUES (null
 mysqli_query($conn,$sql_autor);
 $sql_tytul="INSERT INTO `BiblTytul`(`id_tytul`, `tytul`) VALUES (null,'$tytul')";
 mysqli_query($conn,$sql_tytul);
-header("location=http://php-marcin-gaik.herokuapp.com");
+header("location:http://php-marcin-gaik.herokuapp.com/biblioteka.php");
 ?>
