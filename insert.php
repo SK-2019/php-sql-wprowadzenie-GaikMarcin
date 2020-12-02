4b5d16d077c73915ab9112264c8ce930d8a965a0
GaikMarcin <marcin.gaik5@gmail.com>
2020-12-02 16:02:24 +0100

    update

--- insert.php
+++ insert.php
@@ -7,5 +7,18 @@ $sql_autor="INSERT INTO `BiblAutor`(`id_autor`, `imie`, `nazwisko`) VALUES (null
 mysqli_query($conn,$sql_autor);
 $sql_tytul="INSERT INTO `BiblTytul`(`id_tytul`, `tytul`) VALUES (null,'$tytul')";
 mysqli_query($conn,$sql_tytul);
+$sql_autor2="SELECT id_autor from BiblAutor where nazwisko='$nazwisko'";
+$result1=$conn->query($sql_autor2);
+while($row1=$result1->fetch_assoc()){
+    $autor_id=$row['id_autor'];
+}
+
+$sql_tytul2="SELECT id_tytul from BiblTytul where tytul='$tytul'";
+$result2=$conn->query($sql_tytul2);
+while($row2=$result2->fetch_assoc()){
+    $tytul_id=$row['id_tytul'];
+}
+$sql_krzyz="INSERT INTO `BiblKrzyz`(`id_krzyz`, `id_tytul`, `id_autor`) VALUES (null,'$tytul_id','$autor_id')";
+mysqli_query($conn,$sql_krzyz);
 header("location:http://php-marcin-gaik.herokuapp.com/biblioteka.php");
 ?>
