88a8457c728c8739c2639366e46eaaa68f342233
GaikMarcin <marcin.gaik5@gmail.com>
2020-12-02 16:17:50 +0100

    update

--- insert.php
+++ insert.php
@@ -12,13 +12,13 @@ $sql_autor2="SELECT id_autor from BiblAutor where nazwisko='$nazwisko'";
 $result1=$conn->query($sql_autor2);
 echo($sql_autor2);
 while($row1=$result1->fetch_assoc()){
-    $autor_id=$row['id_autor'];
+    $autor_id=$row1['id_autor'];
 }
 echo($autor_id);
 $sql_tytul2="SELECT id_tytul from BiblTytul where tytul='$tytul'";
 $result2=$conn->query($sql_tytul2);
 while($row2=$result2->fetch_assoc()){
-    $tytul_id=$row['id_tytul'];
+    $tytul_id=$row2['id_tytul'];
 }
 $sql_krzyz="INSERT INTO `BiblKrzyz`(`id_krzyz`, `id_tytul`, `id_autor`) VALUES (null,'$tytul_id','$autor_id')";
 mysqli_query($conn,$sql_krzyz);
