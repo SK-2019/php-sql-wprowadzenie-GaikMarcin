ab9b10a102f4bad77bd8d61626453b00ffee9072
GaikMarcin <marcin.gaik5@gmail.com>
2020-12-02 15:18:41 +0100

    update

---
+++ biblioteka.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+<?php
+require_once("conn.php");
+$result=$conn->query('SELECT id_krzyz as id,tytul,imie,nazwisko FROM `BiblKrzyz`,BiblTytul,BiblAutor where BiblAutor.id_autor=BiblKrzyz.id_autor and BiblTytul.id_tytul=BiblKrzyz.id_tytul');
+echo("<table border=1>");
+echo("<th>id</th>");
+echo("<th>tytul</th>");
+echo("<th>imie</th>");
+echo("<th>nazwisko</th>");    
+    while($row=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$row['id']."</td><td>".$row['tytul']."</td><td>".$row['imie']."</td><td>".$row['nazwisko']."</td>");
+        echo("</tr>");
+    }
+echo("</table>");
+?>    
+</body>
+</html>
