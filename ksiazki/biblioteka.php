27ae323995ed257c6aa0bf77b18242929e2d396f
GaikMarcin <marcin.gaik5@gmail.com>
2020-12-04 08:59:34 +0100

    update

--- ksiazki/biblioteka.php
+++ ksiazki/biblioteka.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="/style1.css">
     <a href="https://github.com/SK-2019/php-sql-wprowadzenie-GaikMarcin"> GitHub </a>
     <div class="nav">
     <a class="nav_link" href="/index.php">Strona Główna</a> 
