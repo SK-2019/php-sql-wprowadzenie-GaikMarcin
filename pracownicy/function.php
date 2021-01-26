b9c5012322fb9d605ce273dba2df990704a20611
GaikMarcin <marcin.gaik5@gmail.com>
2021-01-26 16:26:07 +0100

    update

--- pracownicy/function.php
+++ pracownicy/function.php
@@ -7,7 +7,7 @@
 <span onclick="openNav()">&#9776</span>
 <div class="sidebar" id="mySidenav">
     <a class="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-GaikMarcin"> GitHub </a>
-    <a class="nav_link" href="../assets/index.php">Strona Główna</a>
+    <a class="nav_link" href="../index.php">Strona Główna</a>
     <a class="nav_link" href="pracownicy/pracownicy.php">Pracownicy - wstęp</a>
     <a class="nav_link" href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a>
     <a class="nav_link" href="pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a>
