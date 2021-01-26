1257daeeab6c1486ebbac480ee0df801301741ae
GaikMarcin <marcin.gaik5@gmail.com>
2021-01-26 13:47:49 +0100

    update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="assets/style.css">
 <span onclick="openNav()">&#9776</span>
 <div class="sidebar" id="mySidenav">
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-GaikMarcin"> GitHub </a>
+    <a class="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-GaikMarcin"> GitHub </a>
     <a class="nav_link" href="pracownicy/pracownicy.php">Pracownicy - wstęp</a>
     <a class="nav_link" href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a>
     <a class="nav_link" href="pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a>
@@ -114,6 +114,13 @@ echo("<hr />");
 ?>
 <script>
 function openNav() {
-    document.getElementById("mySidenav").style.left = "0px";
+    if(document.getElementById("mySidenav").style.left=="-250px"){
+        document.getElementById("mySidenav").style.left = "0px"; 
+    }
+    else{
+        document.getElementById("mySidenav").style.left = "-250px"; 
+    }
+    
 }
+
 </script>
