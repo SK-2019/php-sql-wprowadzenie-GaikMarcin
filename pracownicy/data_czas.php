13551a5f5d2581f12d18221395b9c7416bda3365
GaikMarcin <marcin.gaik5@gmail.com>
2021-01-26 14:44:46 +0100

    update

--- pracownicy/data_czas.php
+++ pracownicy/data_czas.php
@@ -2,7 +2,6 @@
 <html>
 <head>
 <link rel="stylesheet" href="../style1.css">
-<a href="https://github.com/SK-2019/php-sql-wprowadzenie-GaikMarcin"> GitHub </a>
 <span onclick="openNav()">&#9776</span>
 <div class="sidebar" id="mySidenav">
     <a class="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-GaikMarcin"> GitHub </a>
