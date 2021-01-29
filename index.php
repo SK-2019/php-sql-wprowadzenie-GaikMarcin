8edeabbf9946b162208dbf1db0e5f9f8d8eab809
Gaik Marcin <marcin.gaik5@gmail.com>
2021-01-29 12:38:36 +0100

    update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <title>Marcin Gaik</title>
 
-<link rel="stylesheet" href="assets/style.css">
+<link rel="stylesheet" href="style.css">
 <span onclick="openNav()">&#9776</span>
 <div class="sidebar" id="mySidenav">
     <a class="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-GaikMarcin"> GitHub </a>
