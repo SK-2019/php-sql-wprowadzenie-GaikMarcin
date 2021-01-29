f70c900f35e88a01216b47c2e41d8251b383f858
Gaik Marcin <marcin.gaik5@gmail.com>
2021-01-29 13:05:12 +0100

    update

--- pracownicy/pracownicy.php
+++ pracownicy/pracownicy.php
@@ -5,7 +5,7 @@
 <title>Marcin Gaik</title>
 <span onclick="openNav()">&#9776</span>
 <div class="sidebar" id="mySidenav">
-    <a class="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-GaikMarcin"> GitHub </a>
+<a class="nav_link" target="_blank" rel="noopener noreferrer" href="https://github.com/SK-2019/php-sql-wprowadzenie-GaikMarcin">GitHub</a>
     <a class="nav_link" href="../index.php">Strona Główna</a>
     <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
     <a class="nav_link" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
