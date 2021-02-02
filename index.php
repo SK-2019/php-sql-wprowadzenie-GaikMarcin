1e40b66dd6a63840168d04e030e6a7c462ee70d8
Gaik Marcin <marcin.gaik5@gmail.com>
2021-02-02 13:29:39 +0100

    update

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 <title>Marcin Gaik</title>
-<link rel="stylesheet" href="assets/style.css">
+<link rel="stylesheet" href="assets/style1.css">
+<link rel="icon" href="https://icons-for-free.com/iconfiles/png/512/part+1+github-1320568339880199515.png" type="image/icon type">
 <span onclick="openNav()">&#9776</span>
 <div class="sidebar" id="mySidenav">
     <a class="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-GaikMarcin"> GitHub </a>
@@ -18,7 +19,7 @@
 <body>
    <h1>Marcin Gaik 2Ti</h1>
 <?php
-   require_once('conn.php');
+   require_once('assets/conn.php');
   
     $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org');
     $result=$conn->query($sql);
