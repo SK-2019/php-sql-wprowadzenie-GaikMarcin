0b3b3f11afa151328f772def5ef379d24d68de47
Gaik Marcin <marcin.gaik5@gmail.com>
2021-02-02 14:41:07 +0100

    .

--- index.php
+++ index.php
@@ -116,12 +116,12 @@ echo("<hr />");
 ?>
 <script>
 function openNav() {
-    if(document.getElementById("mySidenav").style.left=="-250px"){
+    // if(document.getElementById("mySidenav").style.left=="-250px"){
         document.getElementById("mySidenav").style.left = "0px"; 
-    }
-    else{
-        document.getElementById("mySidenav").style.left = "-250px"; 
-    }
+    // }
+    // else{
+    //     document.getElementById("mySidenav").style.left = "-250px"; 
+    // }
     
 }
 </script>
