7188ed692cb27dda41a5f664f6a98d4806b21ced
Gaik Marcin <marcin.gaik5@gmail.com>
2021-02-21 22:37:29 +0100

    update

--- pracownicy/pracownicy.php
+++ pracownicy/pracownicy.php
@@ -8,11 +8,11 @@
 <div class="sidebar" id="mySidenav">
 <a class="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-GaikMarcin" target="_blank">GitHub</a>
     <a class="nav_link" href="../index.php">Strona Główna</a>
-    <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
-    <a class="nav_link" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
-    <a class="nav_link" href="data_czas.php">Data i Czas</a>
-    <a class="nav_link" href="danedobazy.php">DaneDoBazy</a>
-    <a class="nav_link" href="function.php">Function</a>
+    <a class="nav_link" href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a>
+    <a class="nav_link" href="pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a>
+    <a class="nav_link" href="pracownicy/data_czas.php">Data i Czas</a>
+    <a class="nav_link" href="pracownicy/danedobazy.php">DaneDoBazy</a>
+    <a class="nav_link" href="pracownicy/function.php">Function</a>
     <a class="nav_link" href="../biblioteka/biblioteka.php">Biblioteka</a>
 </div>
 </head>
