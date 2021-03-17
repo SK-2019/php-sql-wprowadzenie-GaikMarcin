752f1c542401cc51be70e9b1e3858c36e5f5f3a1
Marcin Gaik <marcin.gaik5@gmail.com>
2021-03-17 11:37:05 +0100

    test

--- assets/conn.php
+++ assets/conn.php
@@ -1,14 +1,8 @@
 <?php
-$servername = $_SERVER['servername'];
-$username = $_SERVER['username'];
-$password = $_SERVER['password'];
-$dbname = $_SERVER['dbname']; 
+$servername="db";
+$username="root";
+$password="root";
+$dbname="tom";
 
-// Create connection (łącze się z moją bazą danych)
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection (sprawdzam połączenie z bazą danych)
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+$conn=new mysqli($servername,$username,$password,$dbname);
 ?>
