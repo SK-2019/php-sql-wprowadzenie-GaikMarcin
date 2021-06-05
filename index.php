5c537e59f3b444dce2e65ea3d0fce6856fd641a5
GaikMarcin <marcin.gaik5@gmail.com>
2021-06-05 14:36:29 +0200

    .

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Marcisn Gaik</title>
+<title>Marcisn Gaik.</title>
 <link rel="stylesheet" href="assets/style1.css">
 <link rel="icon" href="https://www.streamscheme.com/wp-content/uploads/2020/04/pepega.png" type="image/icon type">
 
